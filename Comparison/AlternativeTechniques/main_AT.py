f31eefc3a045d63a62f18365d6fbb810cf067e6f
Yanan Gao <43428644+yanangao1709@users.noreply.github.com>
2025-03-02 23:42:02 +0800

    Update main_AT.py

--- Comparison/AlternativeTechniques/main_AT.py
+++ Comparison/AlternativeTechniques/main_AT.py
@@ -106,55 +106,55 @@ def baselines_one_transmission_throughput_fixed_requests():
     # 5. 修改Agent加载的智能体文件路径, Provisioning/Agent.py 中的self.model.load_state_dict对应的Comparison/TrainedModel下的文件夹名
     # 6. 修改不同规模拓扑结构时，在requests.py文件中修改基于不同拓扑结构的请求集合ALL_REQUESTS
 
-    # request_interval = 200
-    # G_Compare = RouteGenerator.draw(os.getcwd() + "\\..\\..\\Topology\\topology.csv")
-    # requests = ALL_REQUESTS[0:request_interval]
-    # data_volumes = DATA_VOLUMES[0:request_interval]
-    # # 候选路径集
-    # candidate_routes = {}
-    # for r_id in range(len(requests)):
-    #     shortest_paths = k_shortest_paths.k_shortest_paths(G_Compare, requests[r_id][0], requests[r_id][1],
-    #                                                        QNConfig.candidate_route_num, weight=customed_weight)
-    #     while (shortest_paths == None) or (len(shortest_paths[1]) != QNConfig.candidate_route_num):
-    #         requests[r_id] = random.sample(range(1, QNConfig.node_num), 2)
-    #         shortest_paths = k_shortest_paths.k_shortest_paths(G_Compare, requests[r_id][0], requests[r_id][1],
-    #                                                            QNConfig.candidate_route_num, weight=customed_weight)
-    #
-    #     candidate_routes[r_id] = shortest_paths[1]
-    #
-    # copy_requests = deepcopy(requests)
-    # copy_candidate_routes = deepcopy(candidate_routes)
-    # EFiRAP_throughput, EFiRAP_average_memory_used_rate = EFiRAP(copy_requests, data_volumes, copy_candidate_routes)
-    # print('EFiRAP_throughput---------' + str(EFiRAP_throughput) + '-----EFiRAP_AMUR---------' + str(EFiRAP_average_memory_used_rate))
-    # Multi_R_throughput, Multi_R_average_memory_used_rate = Multi_R(copy_requests, data_volumes, copy_candidate_routes)
-    # print('Multi_R_throughput---------' + str(Multi_R_throughput) + '-----Multi_R_AMUR---------' + str(Multi_R_average_memory_used_rate))
-    # SophonFixed_throughput, SophonFixed_average_memory_used_rate = SophonFixed(copy_requests, data_volumes, candidate_routes, request_interval)
-    # print('SophonFixed_throughput---------' + str(SophonFixed_throughput) + '-----SophonFixed_AMUR---------' + str(SophonFixed_average_memory_used_rate))
-    #
-    # print(EFiRAP_throughput)
-    # print(Multi_R_throughput)
-    # print(SophonFixed_throughput)
-    # print("\n")
-    # print(EFiRAP_average_memory_used_rate)
-    # print(Multi_R_average_memory_used_rate)
-    # print(SophonFixed_average_memory_used_rate)
+    request_interval = 200
+    G_Compare = RouteGenerator.draw(os.getcwd() + "\\..\\..\\Topology\\topology.csv")
+    requests = ALL_REQUESTS[0:request_interval]
+    data_volumes = DATA_VOLUMES[0:request_interval]
+    # 候选路径集
+    candidate_routes = {}
+    for r_id in range(len(requests)):
+        shortest_paths = k_shortest_paths.k_shortest_paths(G_Compare, requests[r_id][0], requests[r_id][1],
+                                                           QNConfig.candidate_route_num, weight=customed_weight)
+        while (shortest_paths == None) or (len(shortest_paths[1]) != QNConfig.candidate_route_num):
+            requests[r_id] = random.sample(range(1, QNConfig.node_num), 2)
+            shortest_paths = k_shortest_paths.k_shortest_paths(G_Compare, requests[r_id][0], requests[r_id][1],
+                                                               QNConfig.candidate_route_num, weight=customed_weight)
+    
+        candidate_routes[r_id] = shortest_paths[1]
+    
+    copy_requests = deepcopy(requests)
+    copy_candidate_routes = deepcopy(candidate_routes)
+    EFiRAP_throughput, EFiRAP_average_memory_used_rate = EFiRAP(copy_requests, data_volumes, copy_candidate_routes)
+    print('EFiRAP_throughput---------' + str(EFiRAP_throughput) + '-----EFiRAP_AMUR---------' + str(EFiRAP_average_memory_used_rate))
+    Multi_R_throughput, Multi_R_average_memory_used_rate = Multi_R(copy_requests, data_volumes, copy_candidate_routes)
+    print('Multi_R_throughput---------' + str(Multi_R_throughput) + '-----Multi_R_AMUR---------' + str(Multi_R_average_memory_used_rate))
+    SophonFixed_throughput, SophonFixed_average_memory_used_rate = SophonFixed(copy_requests, data_volumes, candidate_routes, request_interval)
+    print('SophonFixed_throughput---------' + str(SophonFixed_throughput) + '-----SophonFixed_AMUR---------' + str(SophonFixed_average_memory_used_rate))
+    
+    print(EFiRAP_throughput)
+    print(Multi_R_throughput)
+    print(SophonFixed_throughput)
+    print("\n")
+    print(EFiRAP_average_memory_used_rate)
+    print(Multi_R_average_memory_used_rate)
+    print(SophonFixed_average_memory_used_rate)
 
     # 18/27/36/45/54/63/72/81/90/108
-    throughput_rel = {}
-    x = [18,27,36,45,54,63,72,81,90,99,108]
-    throughput_rel['X'] = x
-    throughput_rel['EFiRAP'] = [60,83,77,80,104,128,117,113,135,138,138]
-    throughput_rel['Multi-R'] = [57.49999473,63.50000399,76.49999844,71.99999602,95.49999909,97.00836073,110.9999952,111.5193547,122.4999926,118.3738817,131.999996]
-    throughput_rel['Sophon'] = [78,92,95,102,125,130,132,142,147,155,162]
-    pd.DataFrame(throughput_rel).to_csv(os.getcwd() + '\\Throughput_topology_scale_AT.csv', index=False)
+    # throughput_rel = {}
+    # x = [18,27,36,45,54,63,72,81,90,99,108]
+    # throughput_rel['X'] = x
+    # throughput_rel['EFiRAP'] = [60,83,77,80,104,128,117,113,135,138,138]
+    # throughput_rel['Multi-R'] = [57.49999473,63.50000399,76.49999844,71.99999602,95.49999909,97.00836073,110.9999952,111.5193547,122.4999926,118.3738817,131.999996]
+    # throughput_rel['Sophon'] = [78,92,95,102,125,130,132,142,147,155,162]
+    # pd.DataFrame(throughput_rel).to_csv(os.getcwd() + '\\Throughput_topology_scale_AT.csv', index=False)
 
-    AMUR = {}
-    x = [18,27,36,45,54,63,72,81,90,99,108]
-    AMUR['X'] = x
-    AMUR['EFiRAP'] = [0.4389318,0.409385894,0.356525889,0.307293872,0.315891002,0.33320745,0.326909144,0.285825022,0.311198223,0.28596172,0.28836609]
-    AMUR['Multi-R'] = [0.386710771,0.298122051,0.305483848,0.222026571,0.255880924,0.251307696,0.253951031,0.189106093,0.23520732,0.174046678,0.195870825]
-    AMUR['Sophon'] = [0.603380481,0.486308405,0.478726568,0.444954429,0.407453918,0.388509322,0.397102488,0.382346986,0.377108288,0.378896705,0.380634774]
-    pd.DataFrame(AMUR).to_csv(os.getcwd() + '\\AMUR_topology_scale_AT.csv', index=False)
+    # AMUR = {}
+    # x = [18,27,36,45,54,63,72,81,90,99,108]
+    # AMUR['X'] = x
+    # AMUR['EFiRAP'] = [0.4389318,0.409385894,0.356525889,0.307293872,0.315891002,0.33320745,0.326909144,0.285825022,0.311198223,0.28596172,0.28836609]
+    # AMUR['Multi-R'] = [0.386710771,0.298122051,0.305483848,0.222026571,0.255880924,0.251307696,0.253951031,0.189106093,0.23520732,0.174046678,0.195870825]
+    # AMUR['Sophon'] = [0.603380481,0.486308405,0.478726568,0.444954429,0.407453918,0.388509322,0.397102488,0.382346986,0.377108288,0.378896705,0.380634774]
+    # pd.DataFrame(AMUR).to_csv(os.getcwd() + '\\AMUR_topology_scale_AT.csv', index=False)
 
 
 
