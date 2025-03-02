23615696d1698ebc150de73020e315fb66a67035
Yanan Gao <43428644+yanangao1709@users.noreply.github.com>
2025-03-02 16:37:01 +0800

    Add files via upload

---
+++ Config/TopologyConfig.py
@@ -0,0 +1,21 @@
+node_num = 18
+#
+# node_num = 27
+
+# node_num = 36
+
+# node_num = 45
+
+# node_num = 54
+# ----------------------------------------------------------------
+# node_num = 63
+
+# node_num = 72
+
+# node_num = 81
+
+# node_num = 90
+
+# node_num = 99
+
+# node_num = 108
